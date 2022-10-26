ea51f0bb616900c3a40bf6f4d9d085398c1b38d7
Sehrish <sehrishzeba012@gmail.com>
2022-10-26 21:42:26 +0600

    4th

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
-// const news = require('./data/news.json');
+const courseDetails = require('./data/courseDetails.json');
 
 app.get('/', (req, res) => {
     res.send('Courses API Running');
@@ -15,6 +15,18 @@ app.get('/', (req, res) => {
 app.get('/courses-categories', (req, res) => {
     res.send(categories)
 });
+
+app.get('/course-details/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const selectedCourse = courseDetails.find(n => n._id === id);
+        res.send(selectedCourse);
+    }
+})
+
 // app.get('/category/:id', (req, res) => {
 //     const id = req.params.id;
 
@@ -28,15 +40,6 @@ app.get('/courses-categories', (req, res) => {
 
 // });
 
-// app.get('/news', (req, res) => {
-//     res.send(news);
-// })
-
-// app.get('/news/:id', (req, res) => {
-//     const id = req.params.id;
-//     const selectedNews = news.find(n => n._id === id);
-//     res.send(selectedNews);
-// });
 
 app.listen(port, () => {
     console.log('Tutor me Server running on port', port);
